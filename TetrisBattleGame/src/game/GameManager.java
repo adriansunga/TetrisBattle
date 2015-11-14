6f61916ac147b167a5ace6f94c6935e09e2bb34d
Natalie Mackraz <mackraz@usc.edu>
2015-11-13 18:42:48 -0800

    hi

--- TetrisBattleGame/src/game/GameManager.java
+++ TetrisBattleGame/src/game/GameManager.java
@@ -1,4 +1,5 @@
 package game;
+
 import java.awt.Color;
 import java.awt.Point;
 import java.util.Arrays;
@@ -9,39 +10,43 @@ public class GameManager {
 	private final int matrixHeight = 20;
 	private final int matrixWidth = 10;
 	private Point[] currentPieceLocation;
-	
+
 	public GameManager() {
 		boardTiles = new Color[matrixHeight][matrixWidth];
 		Arrays.fill(boardTiles, Color.GRAY);
 
 	}
-	
+
 	// Check to see if you should send a line
 	private boolean isLineFull() {
-		for (int i =0; i< matrixWidth; i++) {
-			if (boardTiles[matrixHeight -1][i] == Color.GRAY) {
+		for (int i = 0; i < matrixWidth; i++) {
+			if (boardTiles[matrixHeight - 1][i] == Color.GRAY) {
 				return false;
 			}
 		}
 		return true;
+		//nat
 	}
-	
+
 	// Check to see if the piece can move down further
+	// if the spot is occupied and it's not my piece then false
 	private boolean canMoveDown() {
-		// if the spot is occupied and it's not my piece then false
-		
-		
+		for (Point point : currentPieceLocation) {
+			
+		}
 	}
-	
+
 	// if I occupy a certain given spot
 	private boolean isMyPiece(int x, int y) {
 		Point thisSpot = new Point(x, y);
 		for (Point point : currentPieceLocation) {
-			//HEY ADRIAN ;) ;P
+			if (thisSpot.equals(point)) {
+				System.out.println("isMyPiece is returning true");
+				return true;
+			}
 		}
+		System.out.println("isMyPiece is returning false");
+		return false;
 	}
-	
-	
-	
-	
+
 }
