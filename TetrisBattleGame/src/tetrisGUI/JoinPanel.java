0bf65ba65af46adfd83c68df546479cd25636228
Hailey Manuel <hmanuel@usc.edu>
2015-11-22 02:10:04 -0800

    gui for join panel is done

--- TetrisBattleGame/src/tetrisGUI/JoinPanel.java
+++ TetrisBattleGame/src/tetrisGUI/JoinPanel.java
@@ -1,33 +1,47 @@
 package tetrisGUI;
 
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
+import library.FontLibrary;
 import networking.TetrisClient;
 
 public class JoinPanel extends JPanel{
 
 	private static final long serialVersionUID = -3365559486379271363L;
-	private JLabel ipLabel;
-	private JTextField ipTF;
-	private JLabel portLabel;
-	private JTextField portTF;
+	private JLabel ipLabel, portLabel, titleLabel;
+	private JTextField ipTF, portTF;
 	private JButton continueButton;
 	private String username;
 	private CardLayout cardLayout;
-	private JPanel outerPanelForCardLayout;
-	
+	private JPanel outerPanelForCardLayout, leftPanel, rightPanel, tetrisPanel, tfPanel, portPanel, buttonPanel, tf2Panel, ipPanel;
+	Font font = FontLibrary.getFont("fonts/Tetris_Mania_Type.ttf", Font.BOLD, 30);
 	private Image bg;
+	ImageIcon originalButton1 = new ImageIcon("images/tetrisbattle.png");
+	Image img1 = originalButton1.getImage();
+	Image newImage1 = img1.getScaledInstance(originalButton1.getIconWidth()*5/4, originalButton1.getIconHeight()*5/4, java.awt.Image.SCALE_SMOOTH);
+	ImageIcon ButtonImage11 = new ImageIcon(newImage1);
+	ImageIcon originalButton = new ImageIcon("images/pieces/Tetris_I.svg.png");
+	Image img = originalButton.getImage();
+	Image newImage = img.getScaledInstance(originalButton.getIconWidth()/4, originalButton.getIconHeight()/4, java.awt.Image.SCALE_SMOOTH);
+	ImageIcon ButtonImage1 = new ImageIcon(newImage);
 	
 	//constructor
 	public JoinPanel(CardLayout cardLayout, JPanel outerPanelForCardLayout, String username){
@@ -40,20 +54,97 @@ public class JoinPanel extends JPanel{
 	}
 	
 	private void initializeVariables(){
-		ipLabel = new JLabel("IP:");
-		ipTF = new JTextField(20);
+		ipLabel = new JLabel("IP Address:");
+		ipTF = new JTextField(12);
+		tf2Panel = new JPanel();
+		ipPanel = new JPanel();
+		titleLabel = new JLabel(ButtonImage11);
+		tetrisPanel = new JPanel();
+		leftPanel = new JPanel();
+		tfPanel = new JPanel();
+		portPanel = new JPanel();
+		buttonPanel = new JPanel();
+		rightPanel = new JPanel();
+		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
 		portLabel = new JLabel("Port:");
-		portTF = new JTextField(5);
+		portTF = new JTextField(6);
 		continueButton = new JButton("Continue");
+		continueButton.setIcon(ButtonImage1);
+		continueButton.setHorizontalTextPosition(SwingConstants.CENTER);
+		continueButton.setPreferredSize(new Dimension(ButtonImage1.getIconWidth(), ButtonImage1.getIconHeight()));
+		continueButton.setMaximumSize(new Dimension(ButtonImage1.getIconWidth(), ButtonImage1.getIconHeight()));
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		leftPanel.setOpaque(false);
+		rightPanel.setOpaque(false);
+		tetrisPanel.setOpaque(false);
+		tfPanel.setOpaque(false);
+		portPanel.setOpaque(false);
+		buttonPanel.setOpaque(false);
+		tf2Panel.setOpaque(false);
+		ipPanel.setOpaque(false);
+		portTF.setFont(font);
+		portTF.setForeground(Color.BLACK);
+		ipTF.setFont(font);
+		ipTF.setForeground(Color.BLACK);
+		continueButton.setFont(font);
+		portLabel.setFont(font);
+		portLabel.setForeground(Color.WHITE);
+		portTF.setPreferredSize(new Dimension(20, 50));
+		portTF.setMaximumSize(new Dimension(20, 50));
+		portTF.setMinimumSize(new Dimension(20, 50));
+		ipLabel.setFont(font);
+		ipLabel.setForeground(Color.WHITE);
+		//.setPreferredSize(new Dimension(200, 50));
+//		ipTF.setMinimumSize(new Dimension(200, 50));
+		//tfPanel.setPreferredSize(new Dimension(200, 50));
+		tfPanel.setMaximumSize(new Dimension(200, 50));
+		tfPanel.setMinimumSize(new Dimension(200, 50));
+		portPanel.setPreferredSize(new Dimension(200, 50));
+		portPanel.setMaximumSize(new Dimension(200, 50));
+		portPanel.setMinimumSize(new Dimension(200, 50));
+		//tf2Panel.setPreferredSize(new Dimension(200, 50));
+		//tf2Panel.setMaximumSize(new Dimension(200, 50));
+		//tf2Panel.setMinimumSize(new Dimension(200, 50));
+		ipPanel.setPreferredSize(new Dimension(250, 50));
+		ipPanel.setMaximumSize(new Dimension(250, 50));
+		ipPanel.setMinimumSize(new Dimension(250, 50));
+		buttonPanel.setPreferredSize(new Dimension(ButtonImage1.getIconWidth(), ButtonImage1.getIconHeight()));
+		buttonPanel.setMaximumSize(new Dimension(ButtonImage1.getIconWidth(), ButtonImage1.getIconHeight()));
+		buttonPanel.setMinimumSize(new Dimension(ButtonImage1.getIconWidth(), ButtonImage1.getIconHeight()));
+		leftPanel.setPreferredSize(new Dimension(450, 620));
+		leftPanel.setMaximumSize(new Dimension(450, 620));
+		leftPanel.setMinimumSize(new Dimension(450, 620));
+		rightPanel.setPreferredSize(new Dimension(450, 620));
+		rightPanel.setMaximumSize(new Dimension(450, 620));
+		rightPanel.setMinimumSize(new Dimension(450, 620));
 	}
 	
 	private void createGUI(){
-		//TODO need to actually format, but button functionality is here
-		add(ipLabel);
-		add(ipTF);
-		add(portLabel);
-		add(portTF);
-		add(continueButton);
+		tetrisPanel.add(titleLabel, BorderLayout.CENTER);
+		leftPanel.add(tetrisPanel);
+		tf2Panel.add(ipTF, BorderLayout.CENTER);
+		tfPanel.add(portTF, BorderLayout.CENTER);
+		portPanel.add(portLabel, BorderLayout.WEST);
+		ipPanel.add(ipLabel, BorderLayout.WEST);
+		buttonPanel.add(continueButton, BorderLayout.EAST);
+		leftPanel.add(Box.createGlue());
+		leftPanel.add(Box.createGlue());
+		leftPanel.add(Box.createGlue());
+		rightPanel.add(Box.createGlue());
+		rightPanel.add(Box.createGlue());
+		rightPanel.add(Box.createGlue());
+		rightPanel.add(Box.createGlue());
+		rightPanel.add(Box.createGlue());
+		rightPanel.add(ipPanel);
+		rightPanel.add(tf2Panel);
+		rightPanel.add(portPanel);
+		rightPanel.add(tfPanel);
+		rightPanel.add(Box.createGlue());
+		rightPanel.add(buttonPanel);
+		rightPanel.add(Box.createGlue());
+		add(leftPanel);
+		add(rightPanel);
 		
 		ImageIcon image2 = new ImageIcon("images/backgrounds/s.png");
 		bg = image2.getImage();
